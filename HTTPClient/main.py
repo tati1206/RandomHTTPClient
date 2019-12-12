94dd0452fb689a5fa2a4d2a5ddc26ea3a9162d07
tati <mazzasantino1206@gmail.com>
2019-12-12 15:23:50 -0300

    Added an exceptions handler

--- HTTPClient/main.py
+++ HTTPClient/main.py
@@ -4,31 +4,45 @@ import formatters
 from colorama import Fore
 
 def printResults(response):
-	print("Status: " + formatters.formatStatusCode(response.status_code) + "\n")
+	print("\nStatus: " + formatters.formatStatusCode(response.status_code) + "\n")
 	print(Fore.CYAN + "####### Headers ######" + Fore.RESET + "\n")
 	print(formatters.formatHeaders(response.headers))
 	print(Fore.CYAN + "####### Body ######" + Fore.RESET + "\n")
 	encodeResponse = response.encoding
 	print(response.content.decode(encodeResponse))
 
-
 class httpMethodsCommands:
 	def get(self, url, body={}, headers={}, params={}):
-		response = requests.get(url, data=body, headers=headers, params=params)
-		print(response.url)
-		printResults(response)
+		formattedUrl = formatters.formatUrl(url)
+		try:
+			response = requests.get(formattedUrl, data=body, headers=headers, params=params)
+			printResults(response)
+		except Exception as error:
+			print("\nCannot connect to {}\n".format(formattedUrl))
 
 	def post(self, url, body={}, headers={}, params={}):
-		response = requests.post(url, data=body, headers=headers, params=params)
-		printResults(response)
+		formattedUrl = formatters.formatUrl(url)
+		try:
+			response = requests.post(formattedUrl, data=body, headers=headers, params=params)
+			printResults(response)
+		except Exception as error:
+			print("\nCannot connect to {}\n".format(formattedUrl))
 
 	def put(self, url, body={}, headers={}, params={}):
-		response = requests.put(url, data=body, headers=headers, params=params)
-		printResults(response)
+		formattedUrl = formatters.formatUrl(url)
+		try:
+			response = requests.put(formattedUrl, data=body, headers=headers, params=params)
+			printResults(response)
+		except Exception as error:
+			print("\nCannot connect to {}\n".format(formattedUrl))
 
 	def delete(self, url, body={}, headers={}, params={}):
-		response = requests.delete(url, data=body, headers=headers, params=params)
-		printResults(response)
+		formattedUrl = formatters.formatUrl(url)
+		try:
+			response = requests.delete(formattedUrl, data=body, headers=headers, params=params)
+			printResults(response)
+		except Exception as error:
+			print("\nCannot connect to {}\n".format(formattedUrl))
 
 if __name__ == "__main__":
 	fire.Fire(
